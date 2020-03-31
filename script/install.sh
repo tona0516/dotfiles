1ef6c0c535ed2ed6785874743324662274ec619c
tona0516 <tonango.0516@gmail.com>
2020-03-31 23:31:06 +0900

    Fix install.sh

--- script/install.sh
+++ script/install.sh
@@ -26,6 +26,8 @@ check_installed() {
 
 # check if the following commands are installed
 check_installed git
+check_installed curl
+check_installed vim
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -44,5 +46,4 @@ done
 # install vim plugins
 vim +PlugInstall +qall
 
-# relogin zsh
 exec zsh -l
